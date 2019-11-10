010d3225d2ef3a85fa9aa8697881cf545d429249
HuyLy <lyduchuy1204@gmail.com>
2019-11-11 02:29:40 +0700

    fix code new

---
+++ Main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    
+
+    _getch();
+    return 1;
+}
--- swap function.cpp
+++ swap function.cpp
@@ -1,12 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
+
+void swap(int  &a, int &b);
 
 int main()
 {
     int a = 5;
     int b = 16;
-    int temp = a;
-    a = b;
-    b = temp;    
+
+    swap(a,b);
+	printf("a = %d \n",a);
+	printf("b = %d \n",b);
+	
     _getch();
+    return 0;
+}
+
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
